ddfdc7f727f5a991a19244172c24117186bdc270
ugovaretto <ugovaretto@gmail.com>
2024-11-23 12:16:49 +0800

    Sped up gen permuations by removing recursion.

--- manasa-and-stones/lisp/manasa-and-stones.lisp
+++ manasa-and-stones/lisp/manasa-and-stones.lisp
@@ -1,58 +1,76 @@
-(defun get-permutations (elem-kinds num-elements elements &aux (xs (make-list 1)))
-    (if (zerop num-elements)
-        elements
-        (dolist (k elem-kinds (cdr xs))
-          (setf xs (concatenate 'list xs
-                 (get-permutations elem-kinds
-                       (1- num-elements)
-                       (cons k elements)))))))
+(defun gen-permutations (elements num-perm-elements)
+  (do ((perm-stack '())
+       (result-stack '()))
+      ((and (null perm-stack) (not (null result-stack))) result-stack)
+    (let ((c (pop perm-stack))
+          (r '()))
+      (dolist (e elements)
+        (setf r (cons e c))
+        (if (= (length r) num-perm-elements)
+            (push r result-stack)
+            (push r perm-stack))))))
 
-(defun get-perms (elem-kinds num-elements elements &aux (xs (list 0)))
-    (if (zerop num-elements)
-        elements
-        (dolist (k elem-kinds (cdr xs))
-          (nconc xs
-                 (get-perms elem-kinds
-                       (1- num-elements)
-                       (cons k elements))))))
 
-(defun chunks (ls n xs &aux (ts '(nil)))
-  (if (null ls) (return-from chunks (cdr xs)))
-  (dotimes (_ n)
-    (setf ts (cons (car ls) ts))
-    (setf ls (cdr ls)))
-  (nconc xs (list (cdr (reverse ts))))
-  (chunks ls n xs))
-
-(defun reverse-chunks (ls n xs &aux (ts '()))
-  (if (null ls) (return-from reverse-chunks (cdr xs)))
-  (dotimes (_ n)
-    (setf ts (cons (car ls) ts))
-    (setf ls (cdr ls)))
-  (nconc xs (list ts))
-  (reverse-chunks ls n xs))
-
-(defun unique (xs ys)
+(defun unique (xs &optional (ys '()))
   (if (null xs)
-      (reverse ys)
+      ys
       (if (member (car xs) ys)
           (unique (cdr xs) ys)
-          (progn (setf ys (cons (car xs) ys)) (unique (cdr xs) ys)))))
+          (unique (cdr xs) (cons (car xs) ys)))))
 
 (defun calc (n a b)
   (sort
    (unique
     (mapcar (lambda (c) (apply #'+ c))
-            (reverse-chunks
-             (get-permutations (list a b) n '()) n '(0))) ()) #'<))
+             (gen-permutations (list a b) (1- n)))) #'<))
 
-(defun calc-cases (&aux (results '(nil)))
-  (dotimes (i (read) (cdr results))
-    (setf results (concatenate results
-                               (calc (- (read) 1) (read) (read))))))
+(defun calc-cases ()
+  (let ((results '()))
+    (dotimes (i (read) (reverse results))
+      (setf results (cons (calc (read) (read) (read)) results)))))
 
-(defun main ()
+(defun main (args)
+  (declare (ignore args))
   (dolist (r (calc-cases))
     (dolist (i r)
-      (format t "~A " i)
-      (format t "~%"))))
+      (format t "~A " i))
+    (format t "~%")))
+
+;; (main)
+
+;; (defun gen-permutations (elem-kinds num-elements elements &aux (xs '(0)))
+;;     (if (zerop num-elements)
+;;         elements
+;;         (dolist (k elem-kinds (cdr xs))
+;;           (setf xs (concatenate 'list xs
+;;                  (gen-permutations elem-kinds
+;;                        (- num-elements 1)
+;;                        (cons k elements)))))))
+
+
+;; (defun chunks (ls n xs)
+;;   (if (null ls) (return-from chunks xs)
+;;       (let ((ts '()))
+;;         (dotimes (_ n)
+;;           (setf ts (cons (car ls) ts))
+;;           (setf ls (cdr ls)))
+;;         (chunks ls n (cons ts xs)))))
+
+
+
+;; (defun get-perms (elem-kinds num-elements elements &aux (xs (list 0)))
+;;     (if (zerop num-elements)
+;;         elements
+;;         (dolist (k elem-kinds (cdr xs))
+;;           (nconc xs
+;;                  (get-perms elem-kinds
+;;                        (1- num-elements)
+;;                        (cons k elements))))))
+
+;; (defun chunks (ls n xs &aux (ts '(nil)))
+;;   (if (null ls) (return-from chunks (cdr xs)))
+;;   (dotimes (_ n)
+;;     (setf ts (cons (car ls) ts))
+;;     (setf ls (cdr ls)))
+;;   (nconc xs (list (cdr (reverse ts))))
+;;   (chunks ls n xs))
